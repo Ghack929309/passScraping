fb013f43fd977478a7408ba3a3811c17c5620d32
Ghack929309 <jcalixte024@gmail.com>
2023-08-25 08:09:18 -0400

    25 aout

--- data.mjs
+++ data.mjs
@@ -1,51 +1,92 @@
 export const data = [
-  {
-    name: "Jeannette",
-    lastName: "JEan",
-    passportNumber: "R10087494",
-    passportNumberConfirm: "R10087494",
-    email: "clementchris9293@email.com",
-    visaType: "1", // 1 = Visa pou granmoun, 2 = Visa pou minè
-  },
-   {
-    name: "Amarlie",
-    lastName: "Saint Jean",
-    passportNumber: "R10938917",
-    passportNumberConfirm: "R10938917",
-    email: "clementchris9293@email.com",
-    visaType: "1", // 1 = Visa pou granmoun, 2 = Visa pou minè
-  },
-   {
-    name: "Kendji",
-    lastName: "Sanon",
-    passportNumber: "R10943400",
-    passportNumberConfirm: "R10943400",
-    email: "clementchris9293@email.com",
-    visaType: "1", // 1 = Visa pou granmoun, 2 = Visa pou minè
-  },
-   {
-    name: "Andre Paul",
-    lastName: "Chery",
-    passportNumber: "R10246419",
-    passportNumberConfirm: "R10246419",
-    email: "clementchris9293@email.com",
-    visaType: "1", // 1 = Visa pou granmoun, 2 = Visa pou minè
-  },
-   {
-    name: "Emeline",
-    lastName: "Jean Claude",
-    passportNumber: "PP4992888",
-    passportNumberConfirm: "PP4992888",
-    email: "clementchris9293@email.com",
-    visaType: "1", // 1 = Visa pou granmoun, 2 = Visa pou minè
-  },
-   {
-    name: "Finette",
-    lastName: "Cerene",
-    passportNumber: "R11582339",
-    passportNumberConfirm: "R11582339",
-    email: "clementchris9293@email.com",
-    visaType: "1", // 1 = Visa pou granmoun, 2 = Visa pou minè
-  }
-  // add more data
+	{
+		name: "Islande",
+		lastName: "Joseph",
+		passportNumber: "R10956436",
+		passportNumberConfirm: "R10956436",
+		email: "clementchris9293@email.com",
+		visaType: "1", // 1 = Visa pou granmoun, 2 = Visa pou minè
+	},
+	{
+		name: "Jean Remy",
+		lastName: "Belloze",
+		passportNumber: "R10219763",
+		passportNumberConfirm: "R10219763",
+		email: "clementchris9293@email.com",
+		visaType: "1", // 1 = Visa pou granmoun, 2 = Visa pou minè
+	},
+	{
+		name: "Daniella",
+		lastName: "Paulema",
+		passportNumber: "R11003321",
+		passportNumberConfirm: "R11003321",
+		email: "clementchris9293@email.com",
+		visaType: "1", // 1 = Visa pou granmoun, 2 = Visa pou minè
+	},
+	{
+		name: "Eddyson",
+		lastName: "Donis",
+		passportNumber: "R11158988",
+		passportNumberConfirm: "R11158988",
+		email: "clementchris9293@email.com",
+		visaType: "1", // 1 = Visa pou granmoun, 2 = Visa pou minè
+	},
+	{
+		name: "Rosemane",
+		lastName: "St vil",
+		passportNumber: "R11561828",
+		passportNumberConfirm: "R11561828",
+		email: "clementchris9293@email.com",
+		visaType: "1", // 1 = Visa pou granmoun, 2 = Visa pou minè
+	},
+	{
+		name: "Exzecuel",
+		lastName: "Charles",
+		passportNumber: "R11502393",
+		passportNumberConfirm: "R11502393",
+		email: "clementchris9293@email.com",
+		visaType: "1", // 1 = Visa pou granmoun, 2 = Visa pou minè
+	},
+	{
+		name: "Darling",
+		lastName: "Dieu",
+		passportNumber: "PP5529353",
+		passportNumberConfirm: "PP5529353",
+		email: "clementchris9293@email.com",
+		visaType: "1", // 1 = Visa pou granmoun, 2 = Visa pou minè
+	},
+	{
+		name: "Stephanie",
+		lastName: "Chadavoine",
+		passportNumber: "TB4662877",
+		passportNumberConfirm: "TB4662877",
+		email: "clementchris9293@email.com",
+		visaType: "1", // 1 = Visa pou granmoun, 2 = Visa pou minè
+	},
+	{
+		name: "Emiline",
+		lastName: "Jean Claude",
+		passportNumber: "PP4992888",
+		passportNumberConfirm: "PP4992888",
+		email: "clementchris9293@email.com",
+		visaType: "1", // 1 = Visa pou granmoun, 2 = Visa pou minè
+	},
+	{
+		name: "Kenji",
+		lastName: "Sanon",
+		passportNumber: "R109434000",
+		passportNumberConfirm: "R109434000",
+		email: "clementchris9293@email.com",
+		visaType: "1", // 1 = Visa pou granmoun, 2 = Visa pou minè
+	},
+	,
+	{
+		name: "Pexer Bellande",
+		lastName: "Marie Evelyne",
+		passportNumber: "PP4976348",
+		passportNumberConfirm: "PP4976348",
+		email: "clementchris9293@email.com",
+		visaType: "1", // 1 = Visa pou granmoun, 2 = Visa pou minè
+	},
+	// add more data
 ];
